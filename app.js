eab4c66b303feab33c93d8cfa3b47373845fcf7c
Muhammed-Ezz <not.muhammedezz@gmail.com>
2017-10-31 03:33:50 +0200

    Add Registration view & route.

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+
+var registration = require('./routes/registration');
 
 var app = express();
 
@@ -22,8 +21,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', registration);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
