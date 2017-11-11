74c2402920398f5f1b7d863fe0c3be426a89a4b3
tarekalqaddy <tarek.alqaddy@gmail.com>
2017-11-11 21:03:53 +0200

    Use express-validator in back end form validation.

--- routes/register.js
+++ routes/register.js
@@ -1,28 +1,27 @@
+const { check, validationResult } = require('express-validator/check');
 var express = require('express');
 var router = express.Router();
 var db = require('../db');
 
-function validateEmail(email) {
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-}
+router.post('/', [
+  check('username').isLength({ min: 1 }).withMessage('username can\'t be empty'),
+  check('password').isLength({ min: 6 }).withMessage('password minimum length is 6'),
+  check('email').isEmail().withMessage('Invalid Email')
+], function (req, res, next) {
+    var errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.mapped() });
+    }
 
-
-router.post('/', function (req, res, next) {
     var email = req.body.email
     var username = req.body.username
     var password = req.body.password
-    if (validateEmail(email) && username && password) {
-        var query = `insert into User(id, username, password, email) values (null, '${username}', '${password}', '${email}')`
-        db.query(query, (err, rows, fields) => {
-            if (err) res.send(422)
-            // TODO: redirect to departments
-            // res.redirect('../departments')
-        })
-    } else {
-        res.send(403)
-    }
-
+    var query = `insert into User(id, username, password, email) values (null, '${username}', '${password}', '${email}')`
+    db.query(query, (err, rows, fields) => {
+        if (err) res.status(422).json({ errors: err })
+        // TODO: redirect to departments
+        // res.redirect('../departments')
+    })
 })
 
 module.exports = router
